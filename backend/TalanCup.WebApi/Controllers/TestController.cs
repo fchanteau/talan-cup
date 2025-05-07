14fd8a42bfbc96633546a4bf6c2b50498eb75fea
François CHANTEAU <francois.chanteau@talan.com>
2025-05-07 10:48:17 +0200

    feat: add test routes

---
+++ backend/TalanCup.WebApi/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TalanCup.WebApi.Controllers;
+[Route("api/test")]
+[ApiController]
+public class TestController : ApiController
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello from TalanCup");
+    }
+}
